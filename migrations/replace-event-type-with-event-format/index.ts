859e1c9c6b939549e312cc873624c2fdd916bea1
Tamas Palecian <tamas.palecian@gmail.com>
2024-10-01 13:39:14 +0100

    feat(migration): Using filter for migration

--- migrations/replace-event-type-with-event-format/index.ts
+++ migrations/replace-event-type-with-event-format/index.ts
@@ -5,8 +5,8 @@ const to = 'format'
 
 export default defineMigration({
   title: 'Replace event type with event format',
-  documentTypes: ['event'],
-
+  // documentTypes: ['event'],
+  filter: '_type == "event" && defined(eventType) && !defined(format)',
   migrate: {
     document(doc, context) {
       return [at(to, setIfMissing(doc[from])), at(from, unset())]
